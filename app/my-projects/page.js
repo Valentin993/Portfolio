4a0bb4b039a7ea13babb947345c88b50c4ea13ac
Valentin Maystorovski <valentin.maystorovski@students.beaminstitute.org>
2025-04-30 00:37:24 +0200

    added new card name

--- app/my-projects/page.js
+++ app/my-projects/page.js
@@ -82,7 +82,7 @@ export default function Projects() {
                   href="https://spectrum-cocktail-bar.vercel.app/"
                   target="_blank"
                 >
-                  Concept for Bar WebSite
+                  Concept for Bars WebSite
                 </Link>
               </div>
             </div>
